69a1c5ee82c7c8d36fc33bac693af58d6a4bd9bf
AV <i-am-AV@ya.ru>
2020-07-08 21:26:24 +0300

    Initial Commit

---
+++ NetworkService/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetworkService
+//
+//  Created by  Alexander on 08.07.2020.
+//  Copyright © 2020  Alexander. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
